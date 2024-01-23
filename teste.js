1783ce98eb59dd34f514f165f32d878a8ec9e524
Thiago Silva <thiagojklb@gmail.com>
2024-01-23 14:15:46 -0300

    adicionando mais recursos

--- teste.js
+++ teste.js
@@ -1,24 +1,40 @@
 
+let inputArquivo = document.querySelector(".capiturar-produto");
+console.log(inputArquivo)
 
-document.addEventListener('DOMContentLoaded', function () {
-    const inputArquivo = document.querySelector('.capiturar-produto');
+inputArquivo.addEventListener('change', function() {
+    let imagem = inputArquivo.files[0];
+    if (imagem) {
+        let reader = new FileReader();
+        reader.onload = function(e) {
+            let imgElement = document.createElement("img");
+            imgElement.src = e.target.result;
+            document.body.appendChild(imgElement);
+        }
+        reader.readAsDataURL(imagem);
+    }
+});
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const inputArquivo = document.querySelector('.capiturar-produto');
 
  
 
-    // Adiciona um ouvinte de evento 'change' ao input do tipo file
-    inputArquivo.addEventListener('change', function () {
-        // Obtém o valor do input do tipo file (o arquivo selecionado)
-        const arquivoSelecionado = inputArquivo.files[0];
+//     // Adiciona um ouvinte de evento 'change' ao input do tipo file
+//     inputArquivo.addEventListener('change', function () {
+//         // Obtém o valor do input do tipo file (o arquivo selecionado)
+//         const arquivoSelecionado = inputArquivo.files[0];
 
-        console.log(arquivoSelecionado)
+//         console.log(arquivoSelecionado)
 
-        // Verifica se um arquivo foi selecionado
-        if (arquivoSelecionado) {
-            console.log('Nome do arquivo:', arquivoSelecionado.name);
-            console.log('Tamanho do arquivo:', arquivoSelecionado.size, 'bytes');
-            console.log('Tipo do arquivo:', arquivoSelecionado.type);
-        } else {
-            console.log('Nenhum arquivo selecionado');
-        }
-    });
-});
+//         // Verifica se um arquivo foi selecionado
+//         if (arquivoSelecionado) {
+//             console.log('Nome do arquivo:', arquivoSelecionado.name);
+//             console.log('Tamanho do arquivo:', arquivoSelecionado.size, 'bytes');
+//             console.log('Tipo do arquivo:', arquivoSelecionado.type);
+//         } else {
+//             console.log('Nenhum arquivo selecionado');
+//         }
+//     });
+// });
