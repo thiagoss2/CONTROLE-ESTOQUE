1bfdc173c2603fd51ee279a39888c866d98a6ae1
Thiago Silva <thiagojklb@gmail.com>
2024-01-25 14:03:23 -0300

    arrumando o input quantidade

--- overlay.js
+++ overlay.js
@@ -2,12 +2,14 @@ const overlay = document.querySelector(".overlay");
 const blocoFlutunte = document.querySelector(".bloco-funtutante-venda");
 const botaoAdicionar = document.getElementById('cabecalho__btn--despesa-js');
 const botaoFechar = document.querySelector(".botao-fechar-js");
-
+const iconeMenoa = document.querySelector(".icone-menos-js")
+const iconeMais = document.querySelector(".icone-mais-js")
+const inputQnatidade = document.querySelector('bloco-flutuante-venda__quantidade__input ')
 
 console.log(botaoFechar)
 
 
-overlay.addEventListener("click" , function () {
+overlay.addEventListener("click", function () {
 
     overlay.style.display = "none";
     blocoFlutunte.style.display = "none";
@@ -15,17 +17,19 @@ overlay.addEventListener("click" , function () {
 })
 
 
-botaoAdicionar.addEventListener("click" , function () {
-    
+botaoAdicionar.addEventListener("click", function () {
+
     overlay.style.display = "block";
     blocoFlutunte.style.display = "block";
 
-   
-} )
+
+})
 
 
-botaoFechar.addEventListener('click' , function() {
+botaoFechar.addEventListener('click', function () {
     overlay.style.display = "none";
     blocoFlutunte.style.display = "none";
- 
+
 });
+
+
--- teste.js
+++ teste.js
@@ -1,33 +1,20 @@
-
 // let inputArquivo = document.querySelector(".capiturar-produto");
-const elemento = document.querySelector(".bloco-movimentos__titulo").textContent;
-
-console.log(elemento);
-// inputArquivo.addEventListener('change', function(event) {
-//     console.log(event); // Aqui você pode inspecionar o objeto event
-//     // Outras ações com base no evento
-// });
 
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const inputArquivo = document.querySelector('.capiturar-produto');
-
- 
-
-//     // Adiciona um ouvinte de evento 'change' ao input do tipo file
-//     inputArquivo.addEventListener('change', function () {
-//         // Obtém o valor do input do tipo file (o arquivo selecionado)
-//         const arquivoSelecionado = inputArquivo.files[0];
-
-//         console.log(arquivoSelecionado)
-
-//         // Verifica se um arquivo foi selecionado
-//         if (arquivoSelecionado) {
-//             console.log('Nome do arquivo:', arquivoSelecionado.name);
-//             console.log('Tamanho do arquivo:', arquivoSelecionado.size, 'bytes');
-//             console.log('Tipo do arquivo:', arquivoSelecionado.type);
-//         } else {
-//             console.log('Nenhum arquivo selecionado');
+// inputArquivo.addEventListener('change', function() {
+//     let arquivoDeImagem = inputArquivo.files[0];
+//     console.log("teste");
+//     // se a imagem foi selecionada
+//     if (arquivoDeImagem) {
+//         let reader = new FileReader();
+//         console.log(reader.onload)
+//         reader.onload = function(e) {
+//             let imgElement = document.createElement("img");
+//             imgElement.src = e.target.result;
+//             console.log(e.target.result)
+//             document.body.appendChild(imgElement);
 //         }
-//     });
+//         reader.readAsDataURL(arquivoDeImagem);
+//     }
 // });
+
