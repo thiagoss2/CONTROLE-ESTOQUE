5de0982da149a09143a718764d93954c1ebb4a6e
Thiago Silva <thiagojklb@gmail.com>
2024-01-25 21:35:24 -0300

    adicionando recursos e mais metodos

--- overlay.js
+++ overlay.js
@@ -5,10 +5,14 @@ const botaoFechar = document.querySelector(".botao-fechar-js");
 const iconeMenos = document.querySelector(".icone-menos-js");
 const iconeMais = document.querySelector(".icone-mais-js")
 const inputQuantidade = document.querySelector('.bloco-flutuante-venda__quantidade__input');
+const inputCusto = document.querySelector('.bloco-flutuante-venda__custo__input');
 
 let quantidadeProdutos = 0;
 
-console.log(botaoFechar)
+let custoProduto = 0;
+
+
+
 
 
 overlay.addEventListener("click", function () {
@@ -49,3 +53,47 @@ iconeMais.addEventListener('click' , function() {
     quantidadeProdutos++
     inputQuantidade.value = quantidadeProdutos;
 });
+
+
+function formataValorDeMoeda(lingua , paisOrigem , valor) {
+    // Formata como moeda usando o formato específico para o Brasil
+const formatoMoeda = new Intl.NumberFormat(lingua, {
+    style: 'currency',
+    currency: paisOrigem,
+  });   
+
+  return formatoMoeda.format(valor);
+}
+
+
+inputCusto.addEventListener('change' , function () {
+     
+
+
+    inputCusto.value = formataValorDeMoeda('pr-BR' , 'BRL', inputCusto.value); 
+    custoProduto = inputCusto.value;
+    console.log(custoProduto)
+
+
+}   );
+
+
+
+function removeSimboloMoeda(valor) {
+
+// Substituir o símbolo de moeda por uma string vazia
+let valorSemSimbolo = valor.replace('R$', '').trim();
+ return valorSemSimbolo;
+
+
+}
+
+function removeVirgulaDeMoedas(valor){
+   let moeda = removeSimboloMoeda(valor);
+   let moedaSemVirgula = moeda.replace( ',', '.');
+
+   return moedaSemVirgula;
+
+}  
+
+console.log(removeVirgulaDeMoedas("R$ 700.000.000,00"));
