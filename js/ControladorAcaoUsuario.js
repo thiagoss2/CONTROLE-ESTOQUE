2e8f32b44071db78525850b1cd9c0f8aea35f1cd
Thiago Silva <thiagojklb@gmail.com>
2024-03-09 17:28:52 -0300

    construindo orientação a objetos no projeto

--- js/ControladorAcaoUsuario.js
+++ js/ControladorAcaoUsuario.js
@@ -5,4 +5,17 @@
 */ 
 class ControladorAcaoUsuario {
 
+    constructor() {
+        
+    const blocoFlutunte = document.querySelector(".bloco-funtutante-venda");
+    const botaoAdicionar = document.getElementById('cabecalho__btn--despesa-js');
+    const botaoFechar = document.querySelector(".botao-fechar-js");
+    const iconeMenos = document.querySelector(".icone-menos-js");
+    const iconeMais = document.querySelector(".icone-mais-js")
+    const botaoAdicionarProduto = document.querySelector('.bloco-flutuante-venda__botao-adicionar');
+    const imagemArquivo = document.getElementById('imagem-arquivo');
+    const overlay = document.querySelector(".overlay");
+
+    }
+
 }
--- js/ManipuladorDOM.js
+++ js/ManipuladorDOM.js
@@ -1,6 +1,15 @@
 class ManipuladorDOM {
-    constructor() {
-        
-    }
-    
+  constructor() {
+    const inputQuantidade = document.querySelector('.bloco-flutuante-venda__quantidade__input');
+    const inputCusto = document.querySelector('.bloco-flutuante-venda__custo__input');
+    const inputArquivo = document.querySelector('.capiturar-produto');
+    const labelEscolhaArquivo = document.getElementById('label-arquivo');
+    const inputNomeProduto = document.querySelector('.bloco-flutuante-venda__produto-input');
+    const inputCodigoBarras = document.querySelector('.bloco-flutuante-venda__codigo-barras__input')
+    const inputPreco = document.querySelector('.bloco-flutuante-venda__preco__input');
+    const textAreaDescricaoProduto = document.querySelector('.bloco-flutuante-venda__descricao-produto');
+    const custoPrecoProduto = document.getElementById('js-custo-produto');
+    const produtoElementoContainer = document.querySelector('.pesquisa-produto-produto__lista');
+    const quantidadeReferencias = document.querySelector('.total-referencias');
+  }
 }
